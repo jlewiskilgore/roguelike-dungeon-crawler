16bdc71dc71d714c5e4637af04d4e13807860597
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-03-14 18:25:40 -0400

    Randomly set player's starting position

--- src/components/DungeonMap.js
+++ src/components/DungeonMap.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 class DungeonMap extends Component {
 	constructor(props) {
 		super(props);
+
+		// For testing: Replace these with argument values
+		var NUM_OF_MAP_COLUMNS = 20;
+		var NUM_OF_MAP_ROWS = 20;
+
+		// Set random starting position for player
+		var playerStartingRow = Math.floor(Math.random() * (NUM_OF_MAP_ROWS));
+		var playerStartingCol = Math.floor(Math.random() * (NUM_OF_MAP_COLUMNS));
+
+		this.state = { playerCurrentLocation: [playerStartingRow, playerStartingCol] };
 	}
 
 	render() {
+		console.log(this.state.playerCurrentLocation);
+
+		var playerCurrentRow = this.state.playerCurrentLocation[0];
+		var playerCurrentCol = this.state.playerCurrentLocation[1];
+
 		var mapRow;
 		var dungeonMap = [];
 
@@ -15,7 +30,12 @@ class DungeonMap extends Component {
 		for(var i=0; i<numCols; i++) {
 			mapRow = [];
 			for(var j=0; j<numRows; j++) {
-				mapRow.push("    0    ");
+				if(i == playerCurrentCol && j == playerCurrentRow) {
+					mapRow.push("	X	");
+				}
+				else {
+					mapRow.push("    -    ");
+				}
 			}
 			dungeonMap.push(<div className="map-row">{mapRow}</div>);
 		}
